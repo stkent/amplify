add1526df9cc18a177a4be78e9044c7f609a373b
Stuart Kent <stuart@detroitlabs.com>
2015-11-02 14:09:36 -0500

    Beef up logging capabilities

--- library/src/main/java/com/github/stkent/amplify/Logger.java
+++ library/src/main/java/com/github/stkent/amplify/Logger.java
@@ -7,12 +7,42 @@ public final class Logger {
 
     private static final String TAG = "Amplify Library";
 
+    public enum LogLevel {
+        NONE(0),
+        ERROR(1),
+        DEBUG(2);
+
+        private final int verbosity;
+
+        LogLevel(final int verbosity) {
+            this.verbosity = verbosity;
+        }
+
+        public int getVerbosity() {
+            return verbosity;
+        }
+    }
+
+    private static LogLevel logLevel = LogLevel.ERROR;
+
     private Logger() {
 
     }
 
+    public static void setLogLevel(@NonNull final LogLevel logLevel) {
+        Logger.logLevel = logLevel;
+    }
+
     public static void d(@NonNull final String message) {
-        Log.d(TAG, message);
+        if (logLevel.getVerbosity() >= LogLevel.DEBUG.getVerbosity()) {
+            Log.d(TAG, message);
+        }
+    }
+
+    public static void e(@NonNull final String message) {
+        if (logLevel.getVerbosity() >= LogLevel.ERROR.getVerbosity()) {
+            Log.e(TAG, message);
+        }
     }
 
 }
