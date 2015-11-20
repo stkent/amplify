a9784c9da202b75eb2ebc9134c3ee44275b463f8
Stuart Kent <stuart@detroitlabs.com>
2015-11-20 12:43:04 -0500

    Add tests for cooldown days check

---
+++ amplify/src/test/java/com/github/stkent/amplify/tracking/checks/CooldownDaysCheckTest.java
@@ -0,0 +1,58 @@
+package com.github.stkent.amplify.tracking.checks;
+
+import android.annotation.SuppressLint;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CooldownDaysCheckTest {
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatCooldownCheckBlocksPrompt_ifCooldownPeriodHasNotPassed() {
+        // Arrange
+        final int cooldownTimeDays = 7;
+        final int daysSinceLastEvent = 2;
+
+        assert daysSinceLastEvent < cooldownTimeDays;
+
+        final CooldownDaysCheck cooldownDaysCheck = new CooldownDaysCheck(cooldownTimeDays);
+        final long lastEventTime = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(daysSinceLastEvent);
+
+        // Act
+        // todo: figure out what to pass instead of the null context here
+        final boolean checkShouldBlockFeedbackPrompt
+                = cooldownDaysCheck.shouldBlockFeedbackPrompt(lastEventTime, null);
+
+        // Assert
+        assertTrue("Feedback prompt should be blocked if the cooldown period has not passed", checkShouldBlockFeedbackPrompt);
+    }
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatCooldownCheckDoesNotBlockPrompt_ifCooldownPeriodHasPassed() {
+        // Arrange
+        final int cooldownTimeDays = 7;
+        final int daysSinceLastEvent = 9;
+
+        assert daysSinceLastEvent > cooldownTimeDays;
+
+        final CooldownDaysCheck cooldownDaysCheck = new CooldownDaysCheck(cooldownTimeDays);
+        final long lastEventTime = System.currentTimeMillis() - TimeUnit.DAYS.toMillis(daysSinceLastEvent);
+
+        // Act
+        final boolean checkShouldBlockFeedbackPrompt
+                = cooldownDaysCheck.shouldBlockFeedbackPrompt(lastEventTime, null);
+
+        // Assert
+        assertFalse("Feedback prompt should not be blocked if the cooldown period has passed",
+                checkShouldBlockFeedbackPrompt);
+    }
+
+}
