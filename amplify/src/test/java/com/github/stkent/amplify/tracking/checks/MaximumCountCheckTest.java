777c8b6d758713ae8e88ae40253bd5f977e83a31
Stuart Kent <stuart@detroitlabs.com>
2015-11-20 12:48:30 -0500

    Add tests for maximum event count check

---
+++ amplify/src/test/java/com/github/stkent/amplify/tracking/checks/MaximumCountCheckTest.java
@@ -0,0 +1,56 @@
+package com.github.stkent.amplify.tracking.checks;
+
+import android.annotation.SuppressLint;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MaximumCountCheckTest {
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatMaximumCountCheckBlocksPrompt_ifCountThresholdHasBeenExceeded() {
+        // Arrange
+        final int maximumEventCount = 7;
+        final int currentEventCount = 9;
+
+        assert currentEventCount > maximumEventCount;
+
+        final MaximumCountCheck maximumCountCheck = new MaximumCountCheck(maximumEventCount);
+
+        // Act
+        // todo: figure out what to pass instead of the null context here
+        final boolean checkShouldBlockFeedbackPrompt
+                = maximumCountCheck.shouldBlockFeedbackPrompt(currentEventCount, null);
+
+        // Assert
+        assertTrue("Feedback prompt should be blocked if the count threshold has been exceeded",
+                checkShouldBlockFeedbackPrompt);
+    }
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatMaximumCountCheckDoesNotBlockPrompt_ifCountThresholdHasNotBeenExceeded() {
+        // Arrange
+        final int maximumEventCount = 7;
+        final int currentEventCount = 2;
+
+        assert currentEventCount < maximumEventCount;
+
+        final MaximumCountCheck maximumCountCheck = new MaximumCountCheck(maximumEventCount);
+
+        // Act
+        // todo: figure out what to pass instead of the null context here
+        final boolean checkShouldBlockFeedbackPrompt
+                = maximumCountCheck.shouldBlockFeedbackPrompt(currentEventCount, null);
+
+        // Assert
+        assertFalse("Feedback prompt should be blocked if the count threshold has been exceeded",
+                checkShouldBlockFeedbackPrompt);
+    }
+
+}
