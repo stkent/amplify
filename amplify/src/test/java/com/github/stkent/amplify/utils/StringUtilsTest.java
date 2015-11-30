e058fe7ead2ab7087e46a976446f8eb12547098f
Bryan Kelly <bkelly@detroitlabs.com>
2015-11-30 14:47:19 -0500

    Removed static import

--- amplify/src/test/java/com/github/stkent/amplify/utils/StringUtilsTest.java
+++ amplify/src/test/java/com/github/stkent/amplify/utils/StringUtilsTest.java
@@ -24,7 +24,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
