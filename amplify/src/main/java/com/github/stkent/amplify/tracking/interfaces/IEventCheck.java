47bfbcb8d7b8ab253ae6eb5f5eabf984fc816ab0
Stuart Kent <stuart@detroitlabs.com>
2016-01-04 12:40:04 -0500

    Cleanup; add documentation to IEventTracker

--- amplify/src/main/java/com/github/stkent/amplify/tracking/interfaces/IEventCheck.java
+++ amplify/src/main/java/com/github/stkent/amplify/tracking/interfaces/IEventCheck.java
@@ -19,7 +19,8 @@ package com.github.stkent.amplify.tracking.interfaces;
 import android.support.annotation.NonNull;
 
 /**
- * An abstract representation of an event-based prerequisite for prompting the user for feedback.
+ * An abstract representation of an event-dependent check that must be satisfied whenever we prompt
+ * the user for feedback.
  *
  * @param <T> the type of the value tracked by this event (Integer, Long or String)
  */
@@ -27,8 +28,8 @@ public interface IEventCheck<T> {
 
     /**
      * Call this method to determine whether or not to prompt the user for feedback at this time.
-     * @param cachedEventValue the current value associated with the tracked
-     *        event this check is applied to
+     * @param cachedEventValue the current value associated with the tracked event this check is
+     *        associated with
      * @return true if this check is satisfied and should allow the feedback prompt to be shown;
      *         false otherwise
      */
