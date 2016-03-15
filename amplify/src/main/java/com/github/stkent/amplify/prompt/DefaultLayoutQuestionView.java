719ce55c2909f44e4edd024d7651e7f1f601019f
Stuart Kent <skent.dev@gmail.com>
2016-03-15 13:06:15 -0400

    Add comment to clarify button settextcolor

--- amplify/src/main/java/com/github/stkent/amplify/prompt/DefaultLayoutQuestionView.java
+++ amplify/src/main/java/com/github/stkent/amplify/prompt/DefaultLayoutQuestionView.java
@@ -18,7 +18,9 @@ package com.github.stkent.amplify.prompt;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.widget.TextView;
 
 import com.github.stkent.amplify.R;
@@ -40,18 +42,31 @@ final class DefaultLayoutQuestionView extends CustomLayoutQuestionView {
             getSubtitleTextView().setTextColor(config.getSubtitleTextColor());
         }
 
-        if (getPositiveButton() instanceof TextView) {
-            ((TextView) getPositiveButton()).setTextColor(config.getPositiveButtonTextColor());
-        }
 
-        if (getNegativeButton() instanceof TextView) {
-            ((TextView) getNegativeButton()).setTextColor(config.getNegativeButtonTextColor());
-        }
+        setQuoteButtonUnquoteTextColor(getPositiveButton(), config.getPositiveButtonTextColor());
+        setQuoteButtonUnquoteTextColor(getNegativeButton(), config.getNegativeButtonTextColor());
 
         // todo: instead, build appropriate drawables for button fill/border, with pressed states
 
         getPositiveButton().setBackgroundColor(config.getPositiveButtonBackgroundColor());
         getNegativeButton().setBackgroundColor(config.getNegativeButtonBackgroundColor());
+    }
+
+    /**
+     * We are defensive here, because it's not uncommon to make "buttons" out of UI components like
+     * FrameLayouts, say. If we can't cast to a TextView to obtain a setText method, the button text
+     * will be left unchanged from the original layout.
+     *
+     * @param quoteButtonUnquote the "button" whose text color we wish to set
+     * @param color the color we wish to apply
+     */
+    private void setQuoteButtonUnquoteTextColor(
+            @NonNull final View quoteButtonUnquote,
+            @ColorInt final int color) {
+
+        if (quoteButtonUnquote instanceof TextView) {
+            ((TextView) quoteButtonUnquote).setTextColor(color);
+        }
     }
 
 }
