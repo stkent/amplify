4fb0fd92fa78a3ab68488f00eee962e733a4eba7
Stuart Kent <skent.dev@gmail.com>
2016-03-18 17:05:07 -0400

    Add new minimum count event-based rule

---
+++ amplify/src/main/java/com/github/stkent/amplify/tracking/rules/MinimumCountRule.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2015 Stuart Kent
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.stkent.amplify.tracking.rules;
+
+import android.support.annotation.NonNull;
+
+import com.github.stkent.amplify.tracking.interfaces.IEventBasedRule;
+
+public final class MinimumCountRule implements IEventBasedRule<Integer> {
+
+    private final int minimumCount;
+
+    public MinimumCountRule(final int minimumCount) {
+        if (minimumCount <= 0) {
+            throw new IllegalStateException(
+                    "Minimum count rule must be configured with a positive threshold");
+        }
+
+        this.minimumCount = minimumCount;
+    }
+
+    @Override
+    public boolean shouldAllowFeedbackPromptByDefault() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldAllowFeedbackPrompt(@NonNull final Integer cachedEventValue) {
+        return cachedEventValue >= minimumCount;
+    }
+
+    @NonNull
+    @Override
+    public String getDescription() {
+        return "MinimumCountRule with minimum required count of " + minimumCount;
+    }
+
+}
---
+++ amplify/src/test/java/com/github/stkent/amplify/tracking/rules/MinimumCountRuleTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2015 Stuart Kent
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.stkent.amplify.tracking.rules;
+
+import android.annotation.SuppressLint;
+
+import com.github.stkent.amplify.helpers.BaseTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MinimumCountRuleTest extends BaseTest {
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatRuleBlocksPromptIfEventHasNeverOccurred() {
+        // Arrange
+        final int anyPositiveInteger = 1;
+        assert anyPositiveInteger > 0;
+
+        final MinimumCountRule minimumCountRule = new MinimumCountRule(anyPositiveInteger);
+
+        // Act
+        final boolean ruleShouldAllowFeedbackPrompt
+                = minimumCountRule.shouldAllowFeedbackPromptByDefault();
+
+        // Assert
+        assertFalse(
+                "Feedback prompt should be blocked if the associated event has never occurred.",
+                ruleShouldAllowFeedbackPrompt);
+    }
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatRuleAllowsPromptIfCountThresholdHasBeenExceeded() {
+        // Arrange
+        final int minimumEventCount = 7;
+        final int currentEventCount = 9;
+        assert currentEventCount > minimumEventCount;
+
+        final MinimumCountRule minimumCountRule = new MinimumCountRule(minimumEventCount);
+
+        // Act
+        final boolean ruleShouldAllowFeedbackPrompt
+                = minimumCountRule.shouldAllowFeedbackPrompt(currentEventCount);
+
+        // Assert
+        assertTrue(
+                "Feedback prompt should be blocked if the count threshold has been exceeded",
+                ruleShouldAllowFeedbackPrompt);
+    }
+
+    @SuppressLint("Assert")
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void testThatRuleBlocksPromptIfCountThresholdHasNotBeenExceeded() {
+        // Arrange
+        final int minimumEventCount = 7;
+        final int currentEventCount = 2;
+        assert currentEventCount < minimumEventCount;
+
+        final MinimumCountRule minimumCountRule = new MinimumCountRule(minimumEventCount);
+
+        // Act
+        final boolean ruleShouldAllowFeedbackPrompt
+                = minimumCountRule.shouldAllowFeedbackPrompt(currentEventCount);
+
+        // Assert
+        assertFalse(
+                "Feedback prompt should be blocked if the count threshold has not been exceeded",
+                ruleShouldAllowFeedbackPrompt);
+    }
+
+}
